090eb0851a194468316126f538f54b8b93ef3c8c
abhishek9807533-svg <abhishek9807533@gmail.com>
2026-08-14 22:01:55 +0530

    Create Day5(b).c

---
+++ Day5(b).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int seconds, hours, minutes, sec;
+    printf("Enter time in seconds: ");
+    scanf("%d", &seconds);
+    hours = seconds / 3600;
+    seconds = seconds % 3600;
+    minutes = seconds / 60;
+    sec = seconds % 60;
+    printf("Time = %02d:%02d:%02d", hours, minutes, sec);
+    return 0;
+}
