b84e4081c46195db3a0d84ff0c7811adf2f75a1b
abhishek9807533-svg <abhishek9807533@gmail.com>
2026-08-15 22:55:21 +0530

    Create DAY6(a).c

---
+++ DAY6(a).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int main()
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("%d is Even number.\n", num);
+    } else {
+        printf("%d isOdd number.\n", num);
+    }
+    return 0;
+}
